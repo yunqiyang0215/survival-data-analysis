2e58dd30452043cc2a4ab9fe1bdba29286ae288b
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-01-21 16:47:50 -0600

    Generated the four GWAS plots for the paper.

--- analysis/create_coxph_gwas_plots.R
+++ analysis/create_coxph_gwas_plots.R
@@ -1,8 +1,12 @@
-# TO DO: Explain here what this script is for, and how to use it.
+# This was the script used to generate the initial GWAS plots for the
+# asthma fine-mapping section of the paper.
 library(ggplot2)
 library(ggrepel)
 library(cowplot)
 region <- "chr1_150600001_155100000"
+# region <- "chr10_6600001_12200000"
+# region <- "chr12_46000001_48700000"
+# region <- "chr15_59000001_63400000"
 aoa  <- readRDS(sprintf("../output/gwas_surv/aoa_gwas_%s.rds",region))
 coa  <- readRDS(sprintf("../output/gwas_surv/coa_gwas_%s.rds",region))
 res  <- data.frame(id = rownames(aoa),
@@ -32,7 +36,9 @@ p <- ggplot(pdat) +
                   min.segment.length = 0) +
   geom_hline(yintercept = 0,color = "black",linetype = "dotted") +
   scale_x_continuous(breaks = seq(0,200,0.5)) +
-  scale_y_continuous(breaks = seq(-100,100,5)) +
+  scale_y_continuous(breaks = seq(-100,100,2)) +
   labs(x = "position (Mb)",y = "log10(p-value)") +
   theme_cowplot(font_size = 10)
-ggsave("coxph_gwas_plot.pdf",p,height = 3,width = 5)
+print(p)
+ggsave(sprintf("coxph_gwas_plot_%s.pdf",region),p,height = 3,width = 5)
+

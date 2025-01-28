31aa50c346f88257d65d9f79120395e460eb98b0
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-01-28 12:01:18 -0600

    Got Yunqi's code to run in create_coxph_finemap_plots.R.

--- scripts/create_coxph_finemap_plots.R
+++ scripts/create_coxph_finemap_plots.R
@@ -2,19 +2,20 @@
 library(ggplot2)
 library(cowplot)
 library(susieR)
-region <- regions[i]
-res <- readRDS(paste0("/Users/nicholeyang/Downloads/survivalsusie/result/asthma_self_report/result202408/all/fit.susie.",region,".rds"))
-gwas <- readRDS(paste0("/Users/nicholeyang/downloads/survivalsusie/result/gwas_surv/all_gwas_",region,".rds"))
+# 10p14
+gwas <- readRDS(file.path("../output/gwas_surv",
+                          "all_gwas_chr10_6600001_12200000.rds"))
+res <- readRDS(file.path("../output/result202408/all",
+                         "fit.susie.chr10_6600001_12200000.rds"))
 fit <- res[[1]]
 X <- res[[2]]
 class(fit) <- c("susie","list")
 cs <- susie_get_cs(fit,X)
-colors <- rep("#C3C3C3",nrow(gwas))
 pvals <- (-log10(gwas[,"p.value.spa"]))
-plot(pvals,col = colors,xlab = "SNP",ylab = "-log10(p-value)",cex = 0.8,pch = 20,main = paste0(region_name[i],": AA"),ylim = c(0,max(pvals,na.rm = TRUE)+0.1))
-for (j in 1:length(cs$cs)){
+plot(pvals,col = colors,xlab = "SNP",ylab = "-log10(p-value)",cex = 0.8,
+     pch = 20,ylim = c(0,max(pvals,na.rm = TRUE) + 0.1))
+for (j in 1:length(cs$cs)) {
   snps <- colnames(X)[cs$cs[[j]]]
   indx <- which(rownames(gwas) %in% snps)
-  colors[indx] <- cols[j]
-  points(indx,-log10(gwas[indx,"p.value.spa"]),col = cols[j],pch = 20)
+  points(indx,-log10(gwas[indx,"p.value.spa"]),pch = 20)
 }

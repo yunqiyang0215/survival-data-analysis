8bd993775a021bda441968e311b8bfd147998c46
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-01-28 21:53:25 -0600

    I have the create_coxph_finemap_plots.R script working for one example.

--- scripts/create_coxph_finemap_plots.R
+++ scripts/create_coxph_finemap_plots.R
@@ -1,21 +1,65 @@
 # TO DO: Explain here what this script does, and how to use it.
 library(ggplot2)
+library(ggrepel)
 library(cowplot)
 library(susieR)
-# 10p14
+#
+# TESTING: 10p14, AA
+#
+# Load the SPACox association testing results and the fine-mapping
+# results, and align the two sets of results.
+cs_colors <- c("dodgerblue","limegreen","#33a02c","orange")
+pvar <- read.table("../data/geno_regions/chr10_6600001_12200000.pvar.gz",
+                   sep = "\t",header = TRUE,stringsAsFactors = FALSE,
+                   comment.char = "")
 gwas <- readRDS(file.path("../output/gwas_surv",
                           "all_gwas_chr10_6600001_12200000.rds"))
-res <- readRDS(file.path("../output/result202408/all",
-                         "fit.susie.chr10_6600001_12200000.rds"))
-fit <- res[[1]]
-X <- res[[2]]
+res  <- readRDS(file.path("../output/result202408/all",
+                          "fit.susie.chr10_6600001_12200000.rds"))
+fit  <- res[[1]]
+X    <- res[[2]]
+rownames(pvar) <- pvar$ID
+ids  <- sapply(strsplit(rownames(gwas),"_"),"[[",1)
+rownames(gwas) <- ids
+ids  <- sapply(strsplit(colnames(X),"_"),"[[",1)
+pvar <- pvar[ids,]
+gwas <- gwas[ids,]
+
+# Set up the plotting data structure.
+pdat <- data.frame(id    = ids,
+                   label = "",
+                   pos   = pvar$POS,
+                   pval  = gwas[,"p.value.spa"],
+                   CS    = "none",
+                   stringsAsFactors = FALSE)
 class(fit) <- c("susie","list")
+pips <- susie_get_pip(fit)
 cs <- susie_get_cs(fit,X)
-pvals <- (-log10(gwas[,"p.value.spa"]))
-plot(pvals,col = colors,xlab = "SNP",ylab = "-log10(p-value)",cex = 0.8,
-     pch = 20,ylim = c(0,max(pvals,na.rm = TRUE) + 0.1))
-for (j in 1:length(cs$cs)) {
-  snps <- colnames(X)[cs$cs[[j]]]
-  indx <- which(rownames(gwas) %in% snps)
-  points(indx,-log10(gwas[indx,"p.value.spa"]),pch = 20)
+num_cs <- length(cs$cs)
+for (i in 1:num_cs) {
+  snps <- cs$cs[[i]]
+  j <- which.max(pips[snps])
+  j <- snps[j]
+  pdat[snps,"CS"] <- paste0("L",i)
+  pdat[j,"label"] <- paste0(pdat[j,"id"]," (L",i,")")
 }
+pdat <- transform(pdat,
+                  pos  = pos/1e6,
+                  pval = -log10(pval),
+                  CS   = factor(CS,c("none","L1","L2","L3","L4")))
+
+# Create the plot.
+p <- ggplot(mapping = aes(x = pos,y = pval,color = CS,label = label)) +
+  geom_point(data = subset(pdat,CS == "none"),shape=20,size=2,color="black") +
+  geom_point(data = subset(pdat,CS != "none"),shape=20,size=5) +
+  geom_point(data = subset(pdat,CS != "none"),shape=20,size=2,color="black") +
+  geom_text_repel(data = pdat,color = "black",max.overlaps = Inf,size = 3.5,
+                  min.segment.length = 0) +
+  scale_color_manual(values = cs_colors) +
+  scale_y_continuous(breaks = seq(0,100,5)) +
+  labs(x = "base-pair position on chromosome (Mb)",y = "-log10 p-value",
+       title = "10p14, AA") +
+  theme_cowplot(font_size = 12)
+
+# Save the plots in a PDF.
+ggsave("plot.pdf",p,height = 3.5,width = 6)

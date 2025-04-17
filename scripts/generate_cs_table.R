bc224fb56d13c7a88792c12f8e57f63c0c71e6f3
Peter Carbonetto <peter.carbonetto@gmail.com>
2025-04-17 16:26:54 -0500

    Updated generate_cs_table.R for the new coxph-susie results.

--- scripts/generate_cs_table.R
+++ scripts/generate_cs_table.R
@@ -21,24 +21,25 @@ out <- NULL
 n <- nrow(regions)
 for (i in 1:n) {
   region <- regions[i,"filename"]
-  pvar <- read.table(sprintf("../data/geno_regions/%s.pvar",region),sep = "\t",
-                     header = TRUE,stringsAsFactors = FALSE,comment.char = "")
+  pvar <- read.table(sprintf("../data/geno_regions/%s.pvar.gz",region),
+                     sep = "\t",header = TRUE,stringsAsFactors = FALSE,
+                     comment.char = "")
   rownames(pvar) <- pvar$ID
   for (trait in traits) {
     cat("region = ",regions[i,"name"],", trait = ",trait,"\n",sep = "")
     if (trait == "COA") {
       gwas <- data.frame(readRDS(paste0("../data/gwas_surv/coa_gwas_",
                                         region,".rds")))
-      res  <- readRDS(paste0("../result202408/coa/fit.susie.",region,".rds"))
+      res  <- readRDS(paste0("../result202504/coa/fit.susie.",region,".rds"))
     }
     else if (trait == "AOA") {
       gwas <- data.frame(readRDS(paste0("../data/gwas_surv/aoa_gwas_"
                                         ,region,".rds")))
-      res  <- readRDS(paste0("../result202408/aoa/fit.susie.",region,".rds"))
+      res  <- readRDS(paste0("../result202504/aoa/fit.susie.",region,".rds"))
     } else {
       gwas <- data.frame(readRDS(paste0("../data/gwas_surv/all_gwas_",
                                         region,".rds")))
-      res  <- readRDS(paste0("../result202408/all/fit.susie.",region,".rds"))
+      res  <- readRDS(paste0("../result202504/all/fit.susie.",region,".rds"))
     }
     fit <- res[[1]]
     X   <- res[[2]]
